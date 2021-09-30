2ac0fec989edcbd9a8a106df89359f35660df542
Swiatlon <wiercikdoc@gmail.com>
2021-09-30 18:08:03 +0200

    added video progress bar functioning and recreated addEventListeners

--- 11-day-custom-html5-video-player/javascript/main.js
+++ 11-day-custom-html5-video-player/javascript/main.js
@@ -1,9 +1,13 @@
-const playButton =document.querySelector("button[name='playButton']");
+const playButton = document.querySelector("button[name='playButton']");
 const videoBackward = document.querySelector("button[name='backward10sec']");
 const videoForward = document.querySelector("button[name='forward25sec']");
 const volume = document.querySelector("input[name='volume']");
 const videoSpeed = document.querySelector("input[name='speed']");
-const video = document.querySelector('video');
+const video = document.querySelector("video");
+const videoProgressBar = document.querySelector(".progress-bar");
+window.onload = function(){
+    video.volume = 0;
+}
 function playingVideo(){
     if(video.paused){
         video.play();
@@ -13,18 +17,14 @@ function playingVideo(){
         playButton.textContent = "▶️";
     }
 }
-function volumeHandler(e){
-    console.log(e.value/100);
-    video.volume =e.value/100;
+function actualVideoPosition(){
+   const actualPostion = (video.currentTime/video.duration)*100;
+   videoProgressBar.style.width =`${actualPostion}%`;
 }
-videoBackward.addEventListener("click",function(){video.currentTime -= 10;});
-videoForward.addEventListener("click",function(){video.currentTime += 25;});
-volume.addEventListener('mousedown',function(){
-    volume.addEventListener("mousemove", function(){volumeHandler(this)});
-});
-volume.addEventListener('mouseup',function(){
-    // volume.removeEventListener("mousemove",arguments.callee);
-    console.log(getEvent)
-})
-// videoSpeed.addEventListener('m')
 playButton.addEventListener('click',playingVideo);
+video.addEventListener("timeupdate",actualVideoPosition);
+videoBackward.addEventListener("click",function(){video.currentTime -= 10});
+videoForward.addEventListener("click",function(){video.currentTime += 25});
+volume.addEventListener('input',function(){video.volume =this.value});
+videoSpeed.addEventListener('input',function(e){video.playbackRate = this.value});
+
